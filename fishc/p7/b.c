cdc9a1b15d248550ce137d26ca405e09522f3bcd
linshuai <nsity223@gmail.com>
2021-08-29 21:53:06 +0800

    Update: fishc-p7

--- fishc/p7/b.c
+++ fishc/p7/b.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
-
-  
+  int num;
+  printf("please input a number: ");
+  scanf("%d", &num);
+  printf("%d的5次方是: %.2f\n", num, pow(num, 5));
   return 0;
 }
