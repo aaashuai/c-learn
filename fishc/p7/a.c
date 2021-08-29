ebe612be0714bcd20fe0967da6a1bbfcb918dc90
Lin <nsity223@gmail.com>
2021-08-29 21:14:19 +0800

    p7

---
+++ fishc/p7/a.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  char i;
+  unsigned char j;
+  i = 255;
+  j = 255;
+
+  printf("the value of i %d\n", i);
+  printf("the value of j %u\n", j);
+  
+  return 0;
+}
